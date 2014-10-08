156f1fb6fb6d9cf6d8ee532b85cf580bdb94b836
Fleur Broca <brocafleur@gmail.com>
2014-10-08 11:36:20 +0200

    validation 1er exo sans recursive

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,10 +1,28 @@
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  a = min
+  sum = a
+
+  while a != max
+    a += 1
+    sum += a
+  end
+  sum
 end
+
+puts sum_with_while(3, 9)
 
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  sum = 0
+  for a in min..max
+  sum = sum + a
+  end
+  sum
 end
+
+puts sum_with_while(3, 9)
+
 
 def sum_recursive(min, max)
   # CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
