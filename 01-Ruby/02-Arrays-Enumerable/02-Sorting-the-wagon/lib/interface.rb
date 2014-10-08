c1c911c0180286f8ec4514e75bfb5458dc7c143d
Fleur Broca <brocafleur@gmail.com>
2014-10-08 15:51:08 +0200

    validation 2è exo

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -1,5 +1,30 @@
 require_relative "wagon_sort"
 
+# demander au user d'ajouter des noms d'étudiants
+# créer une liste incluant ces noms
+students_list = []
+print "Type a student name: "
+students_name = gets.chomp
+
+while students_name != ""
+  students_list << students_name
+print "Type another student name (or press enter to finish): "
+students_name = gets.chomp
+end
+
+sorted_list = wagon_sort(students_list)
+# créer une liste qui inclut tous les éléments sauf le dernier
+sorted_list_minus_last = sorted_list[0...-1]
+sorted_list_only_last = sorted_list[-1]
+
+puts "Congratulations! Your Wagon has #{students_list.size} students: #{sorted_list_minus_last.join(", ")} and #{sorted_list_only_last}."
+
+
+# [1, 2, 4, 5, 6].reverse.drop(1).reverse
+
+
+
+
 # TODO: Ask the user about students to add to the Wagon.
 #       Remember, to read an input from the command line, use:
 #       - `gets`:  http://www.ruby-doc.org/core-2.1.2/Kernel.html#method-i-gets
