c29f4632bb8a2cbad078062fe89126b15e7d9cff
Fleur Broca <brocafleur@gmail.com>
2014-10-09 10:34:03 +0200

    validation autres exos faux

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,8 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+  puts "Hi"
+  your_message = gets.chomp
+  puts coach_answer(your_message)
+
+  your_message = gets.chomp
+  puts coach_answer_enhanced(your_message)
