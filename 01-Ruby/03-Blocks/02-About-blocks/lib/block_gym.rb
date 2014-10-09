8c8ae4fd946675a2b808c8b05b37d96a2ca831ff
Fleur Broca <brocafleur@gmail.com>
2014-10-09 17:14:40 +0200

    validation exo block gym

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,8 +1,28 @@
 def tag(tag_name, attr = nil)
   # TODO:  Build HTML tags around  content given in the block
   #        The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  # puts tag_name
+
+  if attr != nil
+    # print attr
+    return "<#{tag_name} #{attr[0]}='#{attr[1]}'>#{yield}</#{tag_name}>"
+  end
+  content = yield
+  content
+  return "<#{tag_name}>#{content}</#{tag_name}>"
 end
+
+# test1 = tag("h1") { "Some Title" }
+# print test1
+# "sep"
+test2 = tag("h1", ["id", "title"]) do
+  "Some Title"
+end
+print test2
 
 def timer_for
   # TODO:  Return time taken to execute the given block
+  start_time = Time.now
+  yield
+  Time.now - start_time
 end
