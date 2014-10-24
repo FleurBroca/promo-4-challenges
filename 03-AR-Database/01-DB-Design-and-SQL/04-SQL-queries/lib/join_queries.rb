ed1b50b4472fc41b36bb0454c0c410360227958c
Fleur Broca <brocafleur@gmail.com>
2014-10-24 09:13:37 +0200

    valid

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -2,10 +2,29 @@ require 'sqlite3'
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
+  query = "SELECT tr.name, al.title, ar.name
+  FROM tracks tr
+  JOIN albums al ON (tr.album_id = al.id)
+  JOIN artists ar ON (al.artist_id = ar.id);"
+  a = db.execute(query)
 end
 
 def stats_on(db, genre_name)
-  # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
+  # TODO: For the given category of music, return the number of
+  # tracks and the average song length (as a stats hash)
+  # you have to use a GROUP BY statement with aggregate SQL functions such as
+  # COUNT and AVG. The method should return a Hash of statistics
+  result = db.execute("SELECT COUNT(*),
+    AVG(milliseconds)
+    FROM tracks t
+    JOIN genres g ON (g.id = t.genre_id)
+    WHERE g.name = '#{genre_name}';").flatten
+  length = (result[1] / 60_000).round(2)
+  {
+    category: genre_name,
+    number_of_songs: result[0],
+    avg_length: length
+  }
 end
 
 def top_five_artists(db, genre_name)
