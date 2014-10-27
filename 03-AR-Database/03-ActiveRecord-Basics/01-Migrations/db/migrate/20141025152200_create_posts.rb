9e3f5da68ea66b9683f85df2c7c2b57e13f12db8
Fleur Broca <brocafleur@gmail.com>
2014-10-27 11:45:35 +0100

    valid 1

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,23 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |t|
+      t.string  :name
+      t.string  :url
+      t.timestamps :created_at
+      t.timestamps :updated_at
+    end
   end
 end
+
+class
+
+class AddVotesToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :votes, :integer
+  end
+end
+
+
+
+
