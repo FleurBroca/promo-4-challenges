d0342ab8a29afbb2c178c312b9d5d3d2587ecd7a
Fleur Broca <brocafleur@gmail.com>
2014-10-27 17:39:19 +0100

    validation exo 2

--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
@@ -9,25 +9,43 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    index = Post.all
+    @view.print_posts(index)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    post = Post.create(name: @view.ask_for_name, url: @view.ask_for_url)
+    @view.print_successfully_created(post)
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    post_update = Post.find(@view.ask_for_id)
+    post_update.update_attributes(name: @view.ask_for_name, url: @view.ask_for_url)
+    @view.print_successfully_updated(post_update)
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    post_destroy = Post.find(@view.ask_for_id)
+    post_destroy.destroy
+    @view.print_successfully_destroyed(post_destroy)
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    post = Post.find(@view.ask_for_id)
+    if post.votes == nil
+      post.votes = 0
+    end
+
+    post.votes += 1
+    post.save
+    @view.print_successfully_upvoted(upvote)
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
@@ -1,3 +1,44 @@
 class PostView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+
+  def print_posts(posts)
+    puts "Here is a list of all posts:"
+    posts.each do |post|
+      puts "#{post.id} - '#{post.name}' - '#{post.url}', created at '#{post.created_at}' - '#{post.votes}' votes."
+    end
+  end
+
+  def ask_for_name
+    puts "Title?"
+    print "> "
+    gets.chomp
+  end
+
+  def ask_for_url
+    puts "URL?"
+    print "> "
+    gets.chomp
+  end
+
+  def print_successfully_created(post)
+    puts "Post '#{post.name}' has been successfully created."
+  end
+
+  def print_successfully_updated(post)
+    puts "Post '#{post.name}' has been successfully updated."
+  end
+
+  def print_successfully_destroyed(post)
+    puts "Post '#{post.name}' has been successfully destroyed."
+  end
+
+  def print_successfully_upvoted(post)
+    puts "Post '#{post.name}' has been successfully upvoted."
+  end
+
+  def ask_for_id
+    puts "What ID post are you looking for?"
+    gets.chomp
+  end
+
 end
