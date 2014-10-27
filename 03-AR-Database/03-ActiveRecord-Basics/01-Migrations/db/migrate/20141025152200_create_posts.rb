d2dee40dfcf9702d88539eaa13b1a7baf6d320fc
Fleur Broca <brocafleur@gmail.com>
2014-10-27 17:40:57 +0100

    valid 1er exo

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -4,19 +4,13 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string  :name
       t.string  :url
-      t.timestamps :created_at
-      t.timestamps :updated_at
+      t.timestamps
     end
   end
 end
 
-class
 
-class AddVotesToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :votes, :integer
-  end
-end
+
 
 
 
