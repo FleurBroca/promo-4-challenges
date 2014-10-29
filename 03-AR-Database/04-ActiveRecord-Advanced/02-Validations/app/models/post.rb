d3b8960b0521ff78568c7a8966341e658114fbb0
Fleur Broca <brocafleur@gmail.com>
2014-10-29 12:12:14 +0100

    valid exo

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :user, :name, :url
+  validates :name, length: { minimum: 5 }
+  validates :name, uniqueness: {case_sensitive: false}
+  validates :url, format: { with: /\A.*\..*\z/, message: "invalid url" }
 
   # TODO: Add some validation
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
   has_many :posts
+  validates :username, :email, presence: true
+  validates :username,  uniqueness: true
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/, message: "invalid email" }
+  before_validation :remove_blanks
+
+  private
+
+  def remove_blanks
+    self.email = email.remove_blanks unless email.nil?
+  end
 
   # TODO: Add some validation
 end
